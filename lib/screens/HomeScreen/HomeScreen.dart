cd3ce13d13254c94ddfe3ca6880358bc348d4293
Neoflies <loinguyen@techkids.io>
2020-08-02 11:12:01 +0700

    Update bottom navigation bar

--- lib/screens/HomeScreen/HomeScreen.dart
+++ lib/screens/HomeScreen/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:cogainhatban/screens/SettingsScreen/SettingsScreen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,37 +9,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+    ),
+    SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        body: TabBarView(
-          children: <Widget>[
-            Container(
-              color: Colors.yellow,
-            ),
-            Container(
-              color: Colors.orange,
-            ),
-          ],
-        ),
-        bottomNavigationBar: TabBar(
-          tabs: [
-            Tab(
-              icon: new Icon(Icons.chat_bubble),
-              child: Text('Chats'),
-            ),
-            Tab(
-              icon: new Icon(Icons.settings),
-              child: Text('Settings'),
-            ),
-          ],
-          labelColor: Color(0xff2CB9B0),
-          unselectedLabelColor: Color(0xff949494),
-          indicatorPadding: EdgeInsets.all(5.0),
-          indicatorColor: Colors.transparent,
-        ),
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble),
+            title: Text('Chats'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+        elevation: 22,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xff2CB9B0),
+        onTap: _onItemTapped,
       ),
     );
   }
---
+++ lib/screens/SettingsScreen/SettingsScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  SettingsScreen({Key key}) : super(key: key);
+
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xffF5F6F6),
+      child: Text('Settings', style: TextStyle(color: Colors.amber))
+    );
+  }
+}
